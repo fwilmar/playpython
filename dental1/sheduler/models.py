cc1e1c116e74a6639af89065a60b1ef8c9cdced9
Wilmar Fuquen <wilmarfn@hotmail.com>
2017-03-25 00:08:39 -0500

    Inicial

--- dental1/sheduler/models.py
+++ dental1/sheduler/models.py
@@ -7,9 +7,13 @@ from django.db import models
 
 class Doctor(models.Model):
 	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
 
 class Procedure(models.Model):
 	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
 
 class Order(models.Model):
 	case = models.IntegerField()
--- dental1/sheduler/views.py
+++ dental1/sheduler/views.py
@@ -18,6 +18,15 @@ def index(request):
 	if request.method == 'POST':
 		print 'Entro a POST'
 		form = OrderForm(request.POST)
+		order = Order.objects.create(case=form.case)
+		#order.case =   form.case
+		order.doctor =  form.doctor
+		order.patient =  form.patient
+		order.procedure = form.procedure
+		order.description = form.description
+		order.date_in = form.date_in
+		order.date_out = form.date_out
+		order.save()
 		print 'paso por aca'
 		if form.is_valid():
 			return HttpResponseRedirect('/thanks/')
